080f4d8b4b9a6e20d5d980294400ede81d4407c2
Apple <apple@Apples-MacBook-Pro-9.local>
2019-06-07 14:30:47 -0700

    Complete Iteration 1: 2

--- emojional/emojional/ViewController.swift
+++ emojional/emojional/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let emojis = ["😭": "ugly crying", "😁": "happy"]
 
-    let alertController = UIAlertController.init(title: "Happy", message: "That's awsome", preferredStyle: UIAlertController.Style.alert)
-    
-    
-      let alertController2 = UIAlertController.init(title: "Hot Face", message: "It will get better I promise", preferredStyle: UIAlertController.Style.alert)
+
+    let alertController = UIAlertController.init(title: "\(emojis)", message: "That's awsome", preferredStyle: UIAlertController.Style.alert)
     
     
     
-       let alertController3 = UIAlertController.init(title: "Sleepy", message: "You should get some speep", preferredStyle: UIAlertController.Style.alert)
+//
+//      let alertController2 = UIAlertController.init(title: "Hot Face", message: "It will get better I promise", preferredStyle: UIAlertController.Style.alert)
+//
+//
+//
+//       let alertController3 = UIAlertController.init(title: "Sleepy", message: "You should get some speep", preferredStyle: UIAlertController.Style.alert)
 
     @IBAction func happy(_ sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
         
          alertController.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
         
@@ -28,17 +34,17 @@ class ViewController: UIViewController {
     
     
     @IBAction func redGuy(_ sender: UIButton) {
-        alertController2.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
-        present(alertController2, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
         
     }
     
     
     @IBAction func sleepy(_ sender: UIButton) {
-        alertController3.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
-        present(alertController3, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
     }
     
     
