7c9a7c5da745c8bcdb44d7887debacf1d607dd97
Apple <apple@Apples-MacBook-Pro-9.local>
2019-06-07 14:14:52 -0700

    Complete Iteration1

--- emojional/emojional/ViewController.swift
+++ emojional/emojional/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let alertController = UIAlertController.init(title: "Happy", message: "That's awsome", preferredStyle: UIAlertController.Style.alert)
+    
+    
+      let alertController2 = UIAlertController.init(title: "Hot Face", message: "It will get better I promise", preferredStyle: UIAlertController.Style.alert)
+    
+    
+    
+       let alertController3 = UIAlertController.init(title: "Sleepy", message: "You should get some speep", preferredStyle: UIAlertController.Style.alert)
+
+    @IBAction func happy(_ sender: UIButton) {
+        
+         alertController.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
     }
-
-
+    
+    
+    @IBAction func redGuy(_ sender: UIButton) {
+        alertController2.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController2, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func sleepy(_ sender: UIButton) {
+        alertController3.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController3, animated: true, completion: nil)
+    }
+    
+    
+//    init (alertController){
+//
+//    }
+    
+    
 }
 
